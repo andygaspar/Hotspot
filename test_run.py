add09f8f8f6d2769e060340b2d5c368dd798812c
andrea <andygaspar@libero.it>
2021-01-26 09:04:08 +0100

    istop constraint fixed

--- test_run.py
+++ test_run.py
@@ -7,15 +7,16 @@ import pandas as pd
 
 import numpy as np
 
-np.random.seed(0)
+#np.random.seed(0)
 scheduleType = scheduleMaker.schedule_types(show=False)
 
-num_flights = 40
-num_airlines = 4
-distribution = scheduleType[0]
+num_flights = 20
+num_airlines = 6
+distribution = scheduleType[3]
 print("schedule type: ", distribution)
 
-df = scheduleMaker.df_maker(num_flights, num_airlines, distribution=distribution)
+#df = scheduleMaker.df_maker(num_flights, num_airlines, distribution=distribution)
+df = pd.read_csv("0offers.csv")
 costFun = CostFuns().costFun["realistic"]
 
 print("\nnn bound")
@@ -31,12 +32,17 @@ udpp_model_xp.print_performance()
 
 print("\nistop only pairs")
 xpModel = istop.Istop(udpp_model_xp.get_new_df(), costFun, triples=False)
+
 xpModel.run(True)
+print(xpModel.matches)
 xpModel.print_performance()
+print(xpModel.offers_selected)
 
 
 print("\nistop with triples")
 xpModel = istop.Istop(udpp_model_xp.get_new_df(), costFun, triples=True)
 xpModel.run(True)
+print(xpModel.matches)
 xpModel.print_performance()
+print(xpModel.offers_selected)
 
