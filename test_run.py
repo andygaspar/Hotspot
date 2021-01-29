bcc38f36d59b20a2cd691c8ee2ba14a7180638cd
andrea <andygaspar@libero.it>
2021-01-29 15:52:14 +0100

    reset istop added

--- test_run.py
+++ test_run.py
@@ -1,3 +1,4 @@
+from GlobalFuns.globalFuns import HiddenPrints
 from Istop import istop
 from ModelStructure.ScheduleMaker import scheduleMaker
 from ModelStructure.Costs.costFunctionDict import CostFuns
@@ -10,39 +11,48 @@ import numpy as np
 np.random.seed(0)
 scheduleType = scheduleMaker.schedule_types(show=False)
 
-num_flights = 40
+num_flights = 20
 num_airlines = 4
+
+
 distribution = scheduleType[3]
 print("schedule type: ", distribution)
 
-# df = scheduleMaker.df_maker(num_flights, num_airlines, distribution=distribution)
-df = pd.read_csv("0offers.csv")
+df = scheduleMaker.df_maker(num_flights, num_airlines, distribution=distribution)
+# df = pd.read_csv("0offers.csv")
 costFun = CostFuns().costFun["realistic"]
-
-print("\nnn bound")
-max_model = nnBound.NNBoundModel(df, costFun)
-max_model.run()
-max_model.print_performance()
-
-print("\nudpp")
-udpp_model_xp = udppModel.UDPPmodel(df, costFun)
-udpp_model_xp.run(optimised=True)
-udpp_model_xp.print_performance()
-
-
-print("\nistop only pairs")
-xpModel = istop.Istop(udpp_model_xp.get_new_df(), costFun, triples=False)
-xpModel.run(True)
-xpModel.print_performance()
-
-
-
-
-print("\nistop with triples")
-xpModel = istop.Istop(udpp_model_xp.get_new_df(), costFun, triples=True)
-xpModel.run(True)
-xpModel.print_performance()
-
-
-
-
+#
+# print("\nnn bound")
+# max_model = nnBound.NNBoundModel(df, costFun)
+# max_model.run()
+# max_model.print_performance()
+#
+# print("\nudpp")
+# udpp_model_xp = udppModel.UDPPmodel(df, costFun)
+# udpp_model_xp.run(optimised=True)
+# udpp_model_xp.print_performance()
+#
+#
+# print("\nistop only pairs")
+# xpModel = istop.Istop(udpp_model_xp.get_new_df(), costFun, triples=False)
+# xpModel.run(True)
+# xpModel.print_performance()
+#
+#
+#
+#
+# print("\nistop with triples")
+# xpModel = istop.Istop(udpp_model_xp.get_new_df(), costFun, triples=True)
+# xpModel.run(True)
+# xpModel.print_performance()
+#
+#
+#
+#
+xpModel = istop.Istop(df, costFun, triples=False)
+for i in range(3):
+    df = scheduleMaker.df_maker(num_flights, num_airlines, distribution=distribution)
+    xpModel.run(True)
+    xpModel.print_performance()
+    xpModel.update_object(df, costFun, triples=False)
+    print("fdjksla")
