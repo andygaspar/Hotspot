8333052a61ef12e900e6ef8353de12726698d015
andrea <andygaspar@libero.it>
2021-05-04 17:09:57 +0200

    pre cost fun

--- test_refactor.py
+++ test_refactor.py
@@ -1,6 +1,6 @@
 from GlobalFuns.globalFuns import HiddenPrints
 from Istop import istop
-from ModelStructure.ScheduleMaker import scheduleMaker
+from ScheduleMaker import scheduleMaker
 from ModelStructure.Costs.costFunctionDict import CostFuns
 from ModelStructure.modelStructure import ModelStructure
 from ModelStructure import df_to_schedule as converter
