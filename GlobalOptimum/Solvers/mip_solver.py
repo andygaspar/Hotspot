ac4f5278429b40c371eabfa12b9f3421a976971e
andrea <andygaspar@libero.it>
2021-09-14 10:55:14 +0200

    global done, still to test

---
+++ GlobalOptimum/Solvers/mip_solver.py
@@ -0,0 +1,91 @@
+from typing import Callable, List, Union
+
+import numpy as np
+import pandas as pd
+import mip
+
+import time
+
+
+
+from ..GlobalFuns.globalFuns import HiddenPrints
+from ..ModelStructure import modelStructure as mS
+from ..ModelStructure.Airline import airline as air
+from ..ModelStructure.Flight.flight import Flight
+from ..ModelStructure.Solution import solution
+from ..ModelStructure.Slot.slot import Slot
+from ..libs.uow_tool_belt.general_tools import write_on_file as print_to_void
+
+
+class MipSolverGO(mS.ModelStructure):
+
+    def __init__(self, model, max_time):
+        self.m = mip.Model()
+        self.m.verbose = False
+        self.m.threads = -1
+        self.maxTime = max_time
+        self.x = None
+
+        self.flights = model.flights
+        self.airlines = model.airlines
+        self.slots = model.slots
+
+    def set_variables(self):
+        flight: Flight
+        airline: air.Airline
+        with print_to_void():
+            self.x = np.array([[self.m.add_var(var_type=mip.BINARY) for _ in self.slots] for _ in self.slots])
+
+    def set_constraints(self):
+
+        flight: Flight
+        airline: air.Airline
+        for flight in self.flights:
+
+                self.m += mip.xsum(self.x[flight.index, slot.index] for slot in flight.compatibleSlots) == 1
+
+
+        for slot in self.slots:
+            self.m += mip.xsum(self.x[flight.index, slot.index] for flight in self.flights) <= 1
+
+
+    def set_objective(self):
+        flight: Flight
+
+        self.m.objective = mip.minimize(
+            mip.xsum(self.x[flight.index, slot.index] * flight.cost_fun(slot)
+                   for flight in self.flights for slot in self.slots)
+            )
+
+    def run(self, timing=False, update_flights=False):
+        start = time.time()
+
+        self.set_variables()
+        self.set_constraints()
+        end = time.time() - start
+        if timing:
+            print("Variables and constraints setting time ", end)
+
+        self.set_objective()
+
+        start = time.time()
+        self.m.optimize(max_seconds=self.maxTime)
+        end = time.time() - start
+        if timing:
+            print("Simplex time ", end)
+
+        with print_to_void():
+            solution.make_solution(self)
+
+            for flight in self.flights:
+                if flight.eta > flight.newSlot.time:
+                    print("********************** negative impact *********************************",
+                          flight, flight.eta, flight.newSlot.time)
+
+        if update_flights:
+            self.update_flights()
+
+        return np.array([[el.x for el in col] for col in self.x])
+
+    def reset(self):
+       pass
--- GlobalOptimum/globalOptimum.py
+++ GlobalOptimum/globalOptimum.py
@@ -15,6 +15,8 @@ from ..ModelStructure.Flight.flight import Flight
 from ..ModelStructure.Solution import solution
 from ..ModelStructure.Slot.slot import Slot
 from ..libs.uow_tool_belt.general_tools import write_on_file as print_to_void
+from Solvers import xpress_solver import XpressSolverGO
+from Solvers import mip_solver import MipSolverGO
 
 
 class GlobalOptimum(mS.ModelStructure):
@@ -22,57 +24,22 @@ class GlobalOptimum(mS.ModelStructure):
 
     def __init__(self, slots: List[Slot] = None, flights: List[Flight] = None, alternative_allocation_rule=0.):
         super().__init__(slots=slots, flights=flights, alternative_allocation_rule=alternative_allocation_rule)
-        with print_to_void():
-            self.m = xp.problem()
-        self.x = None
 
-    def set_variables(self):
-        flight: Flight
-        airline: air.Airline
-        with print_to_void():
-            self.x = np.array([[xp.var(vartype=xp.binary) for _ in self.slots] for _ in self.flights])
-            self.m.addVariable(self.x)
 
-    def set_constraints(self):
 
-        flight: Flight
-        airline: air.Airline
-        for flight in self.flights:
-            self.m.addConstraint(
-                xp.Sum(self.x[flight.index, slot.index] for slot in flight.compatibleSlots) == 1
-            )
 
-        for slot in self.slots:
-            self.m.addConstraint(
-                xp.Sum(self.x[flight.index, slot.index] for flight in self.flights) <= 1
-            )
+    def run(self, timing=False, update_flights=False, max_time=120):
+        try:
 
-    def set_objective(self):
-        flight: Flight
-        with print_to_void():
-            self.m.setObjective(
-                xp.Sum(self.x[flight.index, slot.index] * flight.cost_fun(slot)
-                       for flight in self.flights for slot in self.slots)
-            )
+            m = XpressSolverGO(self, max_time)
+            solution_vect = m.run(timing, update_flights)
 
-    def run(self, timing=False, update_flights=False):
-        start = time.time()
-        with print_to_void():
-            self.set_variables()
-            self.set_constraints()
-        end = time.time() - start
-        if timing:
-            print("Variables and constraints setting time ", end)
+        except:
+            print("using MIP")
+            m = MipSolverGO(self, max_time)
 
-        self.set_objective()
 
-        start = time.time()
-        self.m.solve()
-        end = time.time() - start
-        if timing:
-            print("Simplex time ", end)
-
-        self.assign_flights(self.x)
+        self.assign_flights(solution_vect)
         with print_to_void():
             solution.make_solution(self)
 
@@ -84,20 +51,17 @@ class GlobalOptimum(mS.ModelStructure):
         if update_flights:
             self.update_flights()
 
-    def assign_flights(self, sol):
+
+
+    def assign_flights(self, solution_vect):
         with print_to_void():
             for flight in self.flights:
                 #print ('POUIC flight', flight)
                 for slot in self.slots:
                     #print ('POUIC slot', slot, self.m.getSolution(sol[flight.index, slot.index]))
-                    if self.m.getSolution(sol[flight.index, slot.index]) > 0.5:
+                    if solution_vect[flight.index, slot.index] > 0.5:
                         #print ('POUIC match', flight)
                         flight.newSlot = slot
 
-    def reset(self, slots, flights):
-        super().__init__(slots, flights)
-
-        with HiddenPrints():
-            self.m.reset()
-
-        self.x = None
+    # def reset(self, slots, flights):
+    #     super().__init__(slots, flights)
