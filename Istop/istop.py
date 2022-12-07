d80009c8b54edf4328cf2803588f7677a89023ad
Gerald Gurtner <gurtner.gerald@gmail.com>
2022-12-07 19:32:50 +0100

    Removed some prints

--- Istop/istop.py
+++ Istop/istop.py
@@ -107,7 +107,7 @@ class Istop(mS.ModelStructure):
                     if not self.f_in_matched(couple[1]):
                         self.flights_in_matches.append(couple[1])
 
-        print("preprocess concluded in sec:", time.time() - start, "   Number of possible offers: ", len(self.matches))
+        # print("preprocess concluded in sec:", time.time() - start, "   Number of possible offers: ", len(self.matches))
         return len(self.matches) > 0
 
     def run(self, max_offers=5000, timing=False):
@@ -117,19 +117,19 @@ class Istop(mS.ModelStructure):
             g_offer_solver = GurobiOfferSolver(
                 self, offers=self.matches, max_offers=max_offers, time_limit=120, reductions=self.reductions, mip_gap=0)
             self.offers_selected = g_offer_solver.run(timing=timing)
-            print("reduction gurobi ", g_offer_solver.m.getObjective().getValue())
+            # print("reduction gurobi ", g_offer_solver.m.getObjective().getValue())
 
             solution_assignment = self.offerChecker.get_solution_assignment(self.offers_selected)
             self.assign_flights(solution_assignment)
 
-            print("Number of offers selected: ", len(self.offers_selected))
-            print("Offers selected", self.offers_selected)
+            # print("Number of offers selected: ", len(self.offers_selected))
+            # print("Offers selected", self.offers_selected)
 
 
         else:
             for flight in self.flights:
                 flight.newSlot = flight.slot
-        print("Number of offers selected: ", 0)
+       # print("Number of offers selected: ", 0)
         solution.make_solution(self)
         self.offer_solution_maker()
 
